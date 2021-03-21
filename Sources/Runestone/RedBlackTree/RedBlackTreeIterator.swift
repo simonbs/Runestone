db08da3bd8fdd8cf241dc00f9bf0258c450630c7
Simon Støvring <mail@simonbs.dk>
2021-03-21 23:22:53 +0100

    Adds RedBlackTreeIterator

---
+++ Sources/Runestone/RedBlackTree/RedBlackTreeIterator.swift
@@ -0,0 +1,31 @@
+//
+//  RedBlackTreeIterator.swift
+//  
+//
+//  Created by Simon on 21/03/2021.
+//
+
+import Foundation
+
+final class RedBlackTreeIterator<NodeID: RedBlackTreeNodeID, NodeValue: RedBlackTreeNodeValue, NodeData>: IteratorProtocol, LazySequenceProtocol {
+    private let tree: RedBlackTree<NodeID, NodeValue, NodeData>
+    private var node: RedBlackTreeNode<NodeID, NodeValue, NodeData>?
+
+    init(tree: RedBlackTree<NodeID, NodeValue, NodeData>) {
+        self.tree = tree
+        self.node = tree.root.leftMost
+    }
+
+    func next() -> RedBlackTreeNode<NodeID, NodeValue, NodeData>? {
+        let currentNode = node
+        if let rightNode = node?.right {
+            node = rightNode.leftMost
+        } else {
+            while node?.parent != nil, node == node?.parent?.right {
+                node = node?.parent
+            }
+            node = node?.parent
+        }
+        return currentNode
+    }
+}
