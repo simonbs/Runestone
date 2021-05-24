154fa6ba48f11e4e37af8a86fce7e67e4ab78118
Simon Støvring <mail@simonbs.dk>
2021-05-24 17:11:20 +0200

    Enables evaluation of predicates again

--- Sources/Runestone/Language/TreeSitter/TreeSitterTextPredicatesEvaluator.swift
+++ Sources/Runestone/Language/TreeSitter/TreeSitterTextPredicatesEvaluator.swift
@@ -42,39 +42,42 @@ final class TreeSitterTextPredicatesEvaluator {
 
 private extension TreeSitterTextPredicatesEvaluator {
     func evaluate(using parameters: TreeSitterTextPredicate.CaptureEqualsStringParameters) -> Bool {
-        return false
-//        guard let capture = match.capture(forIndex: parameters.captureIndex) else {
-//            return false
-//        }
-//        let contentText = stringView.substring(in: capture.byteRange)
-//        let comparisonResult = contentText == parameters.string
-//        return comparisonResult == parameters.isPositive
+        guard let capture = match.capture(forIndex: parameters.captureIndex) else {
+            return false
+        }
+        let byteRange = capture.byteRange
+        let range = NSRange(location: byteRange.location.value / 2, length: byteRange.length.value / 2)
+        let contentText = stringView.substring(in: range)
+        let comparisonResult = contentText == parameters.string
+        return comparisonResult == parameters.isPositive
     }
 
     func evaluate(using parameters: TreeSitterTextPredicate.CaptureEqualsCaptureParameters) -> Bool {
-        return false
-//        guard let lhsCapture = match.capture(forIndex: parameters.lhsCaptureIndex) else {
-//            return false
-//        }
-//        guard let rhsCapture = match.capture(forIndex: parameters.lhsCaptureIndex) else {
-//            return false
-//        }
-//        let lhsContentText = stringView.substring(in: lhsCapture.byteRange)
-//        let rhsContentText = stringView.substring(in: rhsCapture.byteRange)
-//        let comparisonResult = lhsContentText == rhsContentText
-//        return comparisonResult == parameters.isPositive
+        guard let lhsCapture = match.capture(forIndex: parameters.lhsCaptureIndex) else {
+            return false
+        }
+        guard let rhsCapture = match.capture(forIndex: parameters.lhsCaptureIndex) else {
+            return false
+        }
+        let lhsByteRange = lhsCapture.byteRange
+        let rhsByteRange = rhsCapture.byteRange
+        let lhsRange = NSRange(location: lhsByteRange.location.value / 2, length: rhsByteRange.length.value / 2)
+        let rhsRange = NSRange(location: lhsByteRange.location.value / 2, length: rhsByteRange.length.value / 2)
+        let lhsContentText = stringView.substring(in: lhsRange)
+        let rhsContentText = stringView.substring(in: rhsRange)
+        let comparisonResult = lhsContentText == rhsContentText
+        return comparisonResult == parameters.isPositive
     }
 
     func evaluate(using parameters: TreeSitterTextPredicate.CaptureMatchesPatternParameters) -> Bool {
-        return false
-//        guard let capture = match.capture(forIndex: parameters.captureIndex) else {
-//            return false
-//        }
-//        guard let contentText = stringView.substring(in: capture.byteRange) else {
-//            return false
-//        }
-//        let matchingRange = contentText.range(of: parameters.pattern, options: .regularExpression)
-//        let isMatch = matchingRange != nil
-//        return isMatch == parameters.isPositive
+        guard let capture = match.capture(forIndex: parameters.captureIndex) else {
+            return false
+        }
+        let byteRange = capture.byteRange
+        let range = NSRange(location: byteRange.location.value / 2, length: byteRange.length.value / 2)
+        let contentText = stringView.substring(in: range)
+        let matchingRange = contentText.range(of: parameters.pattern, options: .regularExpression)
+        let isMatch = matchingRange != nil
+        return isMatch == parameters.isPositive
     }
 }
