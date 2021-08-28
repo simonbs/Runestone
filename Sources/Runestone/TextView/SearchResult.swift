aa1cd92c7dd5c1d2a7e66e24b949284cede04567
Simon Støvring <mail@simonbs.dk>
2021-08-28 15:59:01 +0200

    Makes SearchResult conform to Hashable and Equatable

--- Sources/Runestone/TextView/SearchResult.swift
+++ Sources/Runestone/TextView/SearchResult.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
-public struct SearchResult {
+public struct SearchResult: Hashable, Equatable {
     public let range: NSRange
+    public let startLinePosition: LinePosition
+    public let endLinePosition: LinePosition
 }
