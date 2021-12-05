56eaf82371d7d547a8c678968856259d6cfb981f
Simon Støvring <mail@simonbs.dk>
2021-12-05 15:57:32 +0100

    Moves PageGuideView to separate file

--- Sources/Runestone/TextView/TextInput/PageGuideController.swift
+++ Sources/Runestone/TextView/TextInput/PageGuideController.swift
@@ -7,42 +7,6 @@
 
 import UIKit
 
-final class PageGuideView: UIView {
-    var hairlineWidth: CGFloat = 1 / UIScreen.main.scale {
-        didSet {
-            if hairlineWidth != oldValue {
-                setNeedsLayout()
-            }
-        }
-    }
-    var hairlineColor: UIColor? {
-        get {
-            return hairlineView.backgroundColor
-        }
-        set {
-            hairlineView.backgroundColor = newValue
-        }
-    }
-
-    private let hairlineView = UIView()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        isUserInteractionEnabled = false
-        hairlineView.isUserInteractionEnabled = false
-        addSubview(hairlineView)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        hairlineView.frame = CGRect(x: 0, y: 0, width: hairlineWidth, height: bounds.height)
-    }
-}
-
 final class PageGuideController {
     let guideView = PageGuideView()
     var font: UIFont = .systemFont(ofSize: 14) {
---
+++ Sources/Runestone/TextView/TextInput/PageGuideView.swift
@@ -0,0 +1,44 @@
+//
+//  PageGuideView.swift
+//  
+//
+//  Created by Simon on 05/12/2021.
+//
+
+import UIKit
+
+final class PageGuideView: UIView {
+    var hairlineWidth: CGFloat = 1 / UIScreen.main.scale {
+        didSet {
+            if hairlineWidth != oldValue {
+                setNeedsLayout()
+            }
+        }
+    }
+    var hairlineColor: UIColor? {
+        get {
+            return hairlineView.backgroundColor
+        }
+        set {
+            hairlineView.backgroundColor = newValue
+        }
+    }
+
+    private let hairlineView = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        isUserInteractionEnabled = false
+        hairlineView.isUserInteractionEnabled = false
+        addSubview(hairlineView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        hairlineView.frame = CGRect(x: 0, y: 0, width: hairlineWidth, height: bounds.height)
+    }
+}
