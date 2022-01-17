5ea34b89e7903ac50cf6141faa0434110dafd568
Simon Støvring <mail@simonbs.dk>
2022-01-17 20:08:03 +0100

    Simplifies Tree-sitter target

--- Package.swift
+++ Package.swift
@@ -14,8 +14,31 @@ let package = Package(
     ],
     targets: [
         .target(name: "Runestone", dependencies: ["TreeSitter"]),
-        .target(name: "TreeSitter", exclude: ["src"]),
-        .target(name: "TestTreeSitterLanguages"),
+        .target(name: "TreeSitter",
+                path: "tree-sitter/lib",
+                exclude: [
+                    "binding_rust",
+                    "binding_web",
+                    "Cargo.toml",
+                    "README.md",
+                    "src/unicode/README.md",
+                    "src/unicode/LICENSE",
+                    "src/unicode/ICU_SHA",
+                    "src/get_changed_ranges.c",
+                    "src/tree_cursor.c",
+                    "src/stack.c",
+                    "src/node.c",
+                    "src/lexer.c",
+                    "src/parser.c",
+                    "src/language.c",
+                    "src/alloc.c",
+                    "src/subtree.c",
+                    "src/tree.c",
+                    "src/query.c"
+                ],
+                sources: ["src/lib.c"],
+                cSettings: [.unsafeFlags(["-w"])]),
+        .target(name: "TestTreeSitterLanguages", cSettings: [.unsafeFlags(["-w"])]),
         .testTarget(name: "RunestoneTests", dependencies: ["Runestone", "TestTreeSitterLanguages"])
     ]
 )
