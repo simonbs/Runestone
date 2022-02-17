1466cefe6b82e703661c330d23eb186e33d404e9
Simon Støvring <mail@simonbs.dk>
2022-02-17 22:17:28 +0100

    Fixes off-by-one error in line movement

--- Sources/Runestone/TextView/TextInput/LineMovementController.swift
+++ Sources/Runestone/TextView/TextInput/LineMovementController.swift
@@ -76,7 +76,7 @@ private extension LineMovementController {
         guard let line = lineManager.line(containingCharacterAt: location) else {
             return location
         }
-        let lineLocalLocation = max(min(location - line.location, line.data.totalLength - 1), 0)
+        let lineLocalLocation = max(min(location - line.location, line.data.totalLength), 0)
         let lineFragmentNode = currentDelegate.lineMovementController(self, lineFragmentNodeContainingCharacterAt: lineLocalLocation, in: line)
         let lineFragmentLocalLocation = lineLocalLocation - lineFragmentNode.location
         return locationForMoving(lineOffset: lineOffset, fromLocation: lineFragmentLocalLocation, inLineFragmentAt: lineFragmentNode.index, of: line)
