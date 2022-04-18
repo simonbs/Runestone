b4c40c57d4bba4c45f8c117edbeab4c9ca3865a6
Simon Støvring <mail@simonbs.dk>
2022-04-18 08:22:16 +0200

    Fixes typo

--- Sources/Runestone/TextView/TextInput/LayoutManager.swift
+++ Sources/Runestone/TextView/TextInput/LayoutManager.swift
@@ -227,7 +227,7 @@ final class LayoutManager {
         if isLineWrappingEnabled {
             return scrollViewWidth - scrollViewSafeAreaInsets.left - scrollViewSafeAreaInsets.right
         } else {
-            return ceil(textContentWidth + leadingLineSpacing + textContainerInset.right + maximumLineBreakymbolWidth)
+            return ceil(textContentWidth + leadingLineSpacing + textContainerInset.right + maximumLineBreakSymbolWidth)
         }
     }
     private var contentHeight: CGFloat {
@@ -319,7 +319,7 @@ final class LayoutManager {
     private var lineControllers: [DocumentLineNodeID: LineController] = [:]
     private var needsLayout = false
     private var needsLayoutLineSelection = false
-    private var maximumLineBreakymbolWidth: CGFloat {
+    private var maximumLineBreakSymbolWidth: CGFloat {
         if invisibleCharacterConfiguration.showLineBreaks && invisibleCharacterConfiguration.showSoftLineBreaks {
             return max(invisibleCharacterConfiguration.lineBreakSymbolSize.width, invisibleCharacterConfiguration.softLineBreakSymbolSize.width)
         } else if invisibleCharacterConfiguration.showLineBreaks {
@@ -783,7 +783,7 @@ extension LayoutManager {
         lineFragmentController.invisibleCharacterConfiguration = invisibleCharacterConfiguration
         lineFragmentController.lineFragmentView = lineFragmentView
         let lineFragmentOrigin = CGPoint(x: leadingLineSpacing, y: textContainerInset.top + lineYPosition + lineFragment.yPosition)
-        let lineFragmentSize = CGSize(width: lineFragment.scaledSize.width + maximumLineBreakymbolWidth, height: lineFragment.scaledSize.height)
+        let lineFragmentSize = CGSize(width: lineFragment.scaledSize.width + maximumLineBreakSymbolWidth, height: lineFragment.scaledSize.height)
         lineFragmentFrame = CGRect(origin: lineFragmentOrigin, size: lineFragmentSize)
         lineFragmentView.frame = lineFragmentFrame
     }
