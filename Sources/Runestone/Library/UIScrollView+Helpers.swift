0b402eeb41315e550ffa59324c9ce2bb9d3e01d4
Simon Støvring <mail@simonbs.dk>
2022-05-08 22:29:02 +0200

    Caps scroll offset to minimum and maximum offset (#31)

---
+++ Sources/Runestone/Library/UIScrollView+Helpers.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIScrollView {
+    var minimumContentOffset: CGPoint {
+        return CGPoint(x: adjustedContentInset.left * -1, y: adjustedContentInset.top * -1)
+    }
+
+    var maximumContentOffset: CGPoint {
+        let maxX = max(contentSize.width - bounds.width + adjustedContentInset.right, adjustedContentInset.left * -1)
+        let maxY = max(contentSize.height - bounds.height + adjustedContentInset.bottom, adjustedContentInset.top * -1)
+        return CGPoint(x: maxX, y: maxY)
+    }
+}
