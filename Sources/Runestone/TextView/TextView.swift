681268f1b73b972fc0fdd0657beaae6fb90297fc
Simon Støvring <mail@simonbs.dk>
2022-06-07 16:39:14 +0200

    Improves handling of caret positioning

--- Sources/Runestone/TextView/TextView.swift
+++ Sources/Runestone/TextView/TextView.swift
@@ -186,7 +186,7 @@ public final class TextView: UIScrollView {
             }
         }
         set {
-            textInputView.selectedTextRange = IndexedRange(newValue)
+            textInputView.selectedRange = newValue
         }
     }
     /// The current selection range of the text view as a UITextRange.
@@ -829,11 +829,11 @@ public final class TextView: UIScrollView {
         layoutIfNeeded()
         switch selection {
         case .beginning:
-            textInputView.selectedTextRange = IndexedRange(location: line.location, length: 0)
+            textInputView.selectedRange = NSRange(location: line.location, length: 0)
         case .end:
-            textInputView.selectedTextRange = IndexedRange(location: line.data.length, length: 0)
+            textInputView.selectedRange = NSRange(location: line.data.length, length: line.data.length)
         case .line:
-            textInputView.selectedTextRange = IndexedRange(location: line.location, length: line.data.length)
+            textInputView.selectedRange = NSRange(location: line.location, length: line.data.length)
         }
         return true
     }
@@ -898,9 +898,9 @@ private extension TextView {
         if gestureRecognizer.state == .ended {
             willBeginEditingFromNonEditableTextInteraction = false
             let point = gestureRecognizer.location(in: textInputView)
-            let oldSelectedTextRange = textInputView.selectedTextRange
+            let oldSelectedRange = textInputView.selectedRange
             textInputView.moveCaret(to: point)
-            if textInputView.selectedTextRange != oldSelectedTextRange {
+            if textInputView.selectedRange != oldSelectedRange {
                 layoutIfNeeded()
                 editorDelegate?.textViewDidChangeSelection(self)
             }
@@ -935,15 +935,13 @@ private extension TextView {
         }
         if selectedRange.length == 0 {
             textInputView.insertText(characterPair.leading + characterPair.trailing)
-            let newSelectedRange = NSRange(location: range.location + characterPair.leading.count, length: 0)
-            textInputView.selectedTextRange = IndexedRange(newSelectedRange)
+            textInputView.selectedRange = NSRange(location: range.location + characterPair.leading.count, length: 0)
             return true
         } else if let text = textInputView.text(in: selectedRange) {
             let modifiedText = characterPair.leading + text + characterPair.trailing
             let indexedRange = IndexedRange(selectedRange)
             textInputView.replace(indexedRange, withText: modifiedText)
-            let newSelectedRange = NSRange(location: range.location + characterPair.leading.count, length: range.length)
-            textInputView.selectedTextRange = IndexedRange(newSelectedRange)
+            textInputView.selectedRange = NSRange(location: range.location + characterPair.leading.count, length: range.length)
             return true
         } else {
             return false
@@ -970,8 +968,7 @@ private extension TextView {
 
     private func moveCaret(byOffset offset: Int) {
         if let selectedRange = textInputView.selectedRange {
-            let newSelectedRange = NSRange(location: selectedRange.location + offset, length: 0)
-            textInputView.selectedTextRange = IndexedRange(newSelectedRange)
+            textInputView.selectedRange = NSRange(location: selectedRange.location + offset, length: 0)
         }
     }
 
@@ -1176,7 +1173,7 @@ extension TextView: HighlightNavigationControllerDelegate {
         // Layout lines up until the location of the range so we can scroll to it immediately after.
         textInputView.layoutLines(toLocation: range.upperBound)
         scroll(to: range.location)
-        textInputView.selectedTextRange = IndexedRange(range)
+        textInputView.selectedRange = range
         showMenuForText(in: range)
         switch highlightNavigationRange.loopMode {
         case .previousGoesToLast:
