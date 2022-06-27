18972a64f3cd82c9f1261ed6ad9d58f32e75b0fa
Simon Støvring <mail@simonbs.dk>
2022-06-27 16:44:58 +0200

    Removes swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -41,10 +41,3 @@ let package = Package(
         .testTarget(name: "RunestoneTests", dependencies: ["Runestone", "TestTreeSitterLanguages"])
     ]
 )
-
-#if swift(>=5.6)
-// Add the documentation compiler plugin if possible
-package.dependencies.append(
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-)
-#endif
