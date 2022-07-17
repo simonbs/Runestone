b8b94112f753f075c7a11758348404eff9099836
Simon Støvring <mail@simonbs.dk>
2022-07-17 16:10:46 +0200

    Fixes compile issues on Xcode 15

--- Example/Example/Main/MainViewController.swift
+++ Example/Example/Main/MainViewController.swift
@@ -36,9 +36,11 @@ final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+#if compiler(>=5.7)
         if #available(iOS 16, *) {
             contentView.textView.isFindInteractionEnabled = true
         }
+#endif
         contentView.textView.inputAccessoryView = toolsView
         setupMenuButton()
         setupTextView()
@@ -47,6 +49,7 @@ final class MainViewController: UIViewController {
 }
 
 private extension MainViewController {
+#if compiler(>=5.7)
     @available(iOS 16, *)
     @objc private func presentFind() {
         contentView.textView.findInteraction?.presentFindNavigator(showingReplace: false)
@@ -56,6 +59,7 @@ private extension MainViewController {
     @objc private func presentFindAndReplace() {
         contentView.textView.findInteraction?.presentFindNavigator(showingReplace: true)
     }
+#endif
 
     private func setupTextView() {
         let text = UserDefaults.standard.text ?? ""
@@ -78,6 +82,7 @@ private extension MainViewController {
 
     private func makeFeaturesMenu() -> UIMenu {
         var children: [UIMenuElement] = []
+#if compiler(>=5.7)
         if #available(iOS 16, *) {
             children += [
                 UIMenu(options: .displayInline, children: [
@@ -90,6 +95,7 @@ private extension MainViewController {
                 ])
             ]
         }
+#endif
         children += [
             UIAction(title: "Go to Line") { [weak self] _ in
                 self?.presentGoToLineAlert()
