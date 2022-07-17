320d4dbb848a76cd732f4cba33c41219c66248db
Simon Støvring <mail@simonbs.dk>
2022-07-17 16:02:39 +0200

    Adds documentation

--- Sources/Runestone/TextView/Theme.swift
+++ Sources/Runestone/TextView/Theme.swift
@@ -50,6 +50,15 @@ public protocol Theme: AnyObject {
     /// See <doc:CreatingATheme> for more information on higlight names.
     func shadow(for highlightName: String) -> NSShadow?
 #if compiler(>=5.7)
+    /// Highlighted range for a text range matching a search query.
+    ///
+    /// This function is called when highlighting a search result that was found using the standard find/replace interaction enabled using <doc:TextView/isFindInteractionEnabled>.
+    ///
+    /// Return `nil` to prevent highlighting the range.
+    /// - Parameters:
+    ///   - foundTextRange: The text range matching a search query.
+    ///   - style: Style used to decorate the text.
+    /// - Returns: The object used for highlighting the provided text range, or `nil` if the range should not be highlighted.
     @available(iOS 16, *)
     func highlightedRange(forFoundTextRange foundTextRange: NSRange, ofStyle style: UITextSearchFoundTextStyle) -> HighlightedRange?
 #endif
