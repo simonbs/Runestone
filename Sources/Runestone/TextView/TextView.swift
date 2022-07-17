a8c7ea8386d0a60824fe56067bdf1589366d9300
Simon Støvring <mail@simonbs.dk>
2022-07-17 15:48:28 +0200

    Supports scrolling to a range of text

--- Sources/Runestone/TextView/TextView.swift
+++ Sources/Runestone/TextView/TextView.swift
@@ -1047,6 +1047,23 @@ extension TextView {
     }
 }
 
+extension TextView {
+    func scroll(to range: NSRange) {
+        let upperCaretRect = textInputView.caretRect(at: range.upperBound)
+        let lowerContentOffset = contentOffset(forScrollingToLocation: range.lowerBound)
+        let viewportWidth = frame.width - gutterWidth
+        let distanceOutOfScreen = upperCaretRect.minX - (lowerContentOffset.x + viewportWidth)
+        if distanceOutOfScreen > 0 {
+            // Scroll to reveal the entire range on the X-axis.
+            let offsetX = lowerContentOffset.x + min(distanceOutOfScreen, viewportWidth)
+            let cappedOffsetX = min(max(offsetX, minimumContentOffset.x), maximumContentOffset.x)
+            contentOffset = CGPoint(x: cappedOffsetX, y: lowerContentOffset.y)
+        } else {
+            contentOffset = lowerContentOffset
+        }
+    }
+}
+
 private extension TextView {
     @objc private func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
         guard isSelectable else {
@@ -1151,6 +1168,13 @@ private extension TextView {
     }
 
     private func scroll(to location: Int) {
+        let newContentOffset = contentOffset(forScrollingToLocation: location)
+        if newContentOffset != contentOffset {
+            setContentOffset(newContentOffset, animated: false)
+        }
+    }
+
+    private func contentOffset(forScrollingToLocation location: Int) -> CGPoint {
         let caretRect = textInputView.caretRect(at: location)
         let viewportMinX = contentOffset.x + automaticScrollInset.left + gutterWidth
         let viewportMinY = contentOffset.y + automaticScrollInset.top
@@ -1178,10 +1202,7 @@ private extension TextView {
         }
         let cappedXOffset = min(max(preferredContentOffset.x, minimumContentOffset.x), maximumContentOffset.x)
         let cappedYOffset = min(max(preferredContentOffset.y, minimumContentOffset.y), maximumContentOffset.y)
-        let cappedContentOffset = CGPoint(x: cappedXOffset, y: cappedYOffset)
-        if cappedContentOffset != contentOffset {
-            setContentOffset(cappedContentOffset, animated: false)
-        }
+        return CGPoint(x: cappedXOffset, y: cappedYOffset)
     }
 
     private func installEditableInteraction() {
@@ -1323,7 +1344,7 @@ extension TextView: HighlightNavigationControllerDelegate {
         _ = textInputView.becomeFirstResponder()
         // Layout lines up until the location of the range so we can scroll to it immediately after.
         textInputView.layoutLines(toLocation: range.upperBound)
-        scroll(to: range.location)
+        scroll(to: range)
         textInputView.selectedRange = range
         textInputView.presentEditMenuForText(in: range)
         switch highlightNavigationRange.loopMode {
@@ -1372,7 +1393,7 @@ extension TextView: KeyboardObserverDelegate {
                           keyboardWillShowWithHeight keyboardHeight: CGFloat,
                           animation: KeyboardObserver.Animation?) {
         if isAutomaticScrollEnabled, let newRange = textInputView.selectedRange, newRange.length == 0 {
-            scroll(to: newRange.location)
+            scroll(to: newRange)
         }
     }
 }
