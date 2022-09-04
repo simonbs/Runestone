3c9b6148475dd5728b7c3ea62b72eb36e19062e5
Simon Støvring <mail@simonbs.dk>
2022-09-04 17:01:55 +0200

    Implements isPosition(_:atLineBoundaryInDirection:) (#168)

--- Tests/RunestoneTests/TextInputStringTokenizerTests.swift
+++ Tests/RunestoneTests/TextInputStringTokenizerTests.swift
@@ -105,6 +105,46 @@ extension TextInputStringTokenizerTests {
         let indexedPosition = position as! IndexedPosition
         XCTAssertEqual(indexedPosition.index, 290)
     }
+
+    func testBeginningOfDocumentIsAtBoundary() {
+        let tokenizer = makeTokenizer()
+        let position = IndexedPosition(index: 0)
+        let textDirection = UITextDirection(rawValue: UITextStorageDirection.backward.rawValue)
+        let isAtBoundary = tokenizer.isPosition(position, atBoundary: .line, inDirection: textDirection)
+        XCTAssertTrue(isAtBoundary)
+    }
+
+    func testEndOfDocumentIsAtBoundary() {
+        let tokenizer = makeTokenizer()
+        let position = IndexedPosition(index: 457)
+        let textDirection = UITextDirection(rawValue: UITextStorageDirection.forward.rawValue)
+        let isAtBoundary = tokenizer.isPosition(position, atBoundary: .line, inDirection: textDirection)
+        XCTAssertTrue(isAtBoundary)
+    }
+
+    func testBeginningOfLineFragmentIsAtBoundary() {
+        let tokenizer = makeTokenizer()
+        let position = IndexedPosition(index: 35)
+        let textDirection = UITextDirection(rawValue: UITextStorageDirection.backward.rawValue)
+        let isAtBoundary = tokenizer.isPosition(position, atBoundary: .line, inDirection: textDirection)
+        XCTAssertFalse(isAtBoundary)
+    }
+
+    func testEndOfLineFragmentIsAtBoundary() {
+        let tokenizer = makeTokenizer()
+        let position = IndexedPosition(index: 87)
+        let textDirection = UITextDirection(rawValue: UITextStorageDirection.backward.rawValue)
+        let isAtBoundary = tokenizer.isPosition(position, atBoundary: .line, inDirection: textDirection)
+        XCTAssertFalse(isAtBoundary)
+    }
+
+    func testMiddleOfLineFragmentIsNotAtBoundary() {
+        let tokenizer = makeTokenizer()
+        let position = IndexedPosition(index: 35)
+        let textDirection = UITextDirection(rawValue: UITextStorageDirection.backward.rawValue)
+        let isAtBoundary = tokenizer.isPosition(position, atBoundary: .line, inDirection: textDirection)
+        XCTAssertFalse(isAtBoundary)
+    }
 }
 
 // MARK: - Movement in Paragraphs
