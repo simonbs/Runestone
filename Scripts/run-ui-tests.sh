132ecb4c3b3434ebf73ae3af6324eff2f38c4f7a
Simon Støvring <mail@simonbs.dk>
2022-10-29 13:43:11 +0200

    Adds comments

--- Scripts/run-ui-tests.sh
+++ Scripts/run-ui-tests.sh
@@ -3,10 +3,13 @@ SIMULATOR_NAME="UI Test (Korean)"
 SCHEME="Example"
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PROJECT_PATH="${SCRIPT_PATH}/../Example/Example.xcodeproj"
+# Disable "Use the Same Keyboard Language as macOS" in Simulator.app.
 defaults write com.apple.iphonesimulator EnableKeyboardSync -bool NO
+# Create the simulator we will use for the tests.
 xcrun simctl create "${SIMULATOR_NAME}" "iPhone 8" 2> /dev/null
+# Find the UDID of the newly created simulator.
 SIMULATOR_UDID=`xcrun simctl list --json devices | jq -r ".devices | flatten | .[] | select(.name == \"${SIMULATOR_NAME}\").udid"`
-DESTINATION="platform=iOS Simulator,id=${SIMULATOR_UDID}"
+# Edit the simulator's .GlobalPreferences.plist to use the Korean language.
 find ~/Library/Developer/CoreSimulator/Devices/${SIMULATOR_UDID} -type d -maxdepth 0\
   -exec /usr/libexec/PlistBuddy\
   -c "Add :AppleKeyboardsExpanded integer 1"\
@@ -17,6 +20,8 @@ find ~/Library/Developer/CoreSimulator/Devices/${SIMULATOR_UDID} -type d -maxdep
   -c "Add :AppleKeyboards array"\
   -c "Add :AppleKeyboards:0 string ko_KR@Sw=Korean;hw=Automatic"\
   {}/data/Library/Preferences/.GlobalPreferences.plist \;
-xcodebuild build-for-testing -project $PROJECT_PATH -scheme $SCHEME -sdk iphonesimulator -destination "${DESTINATION}"
-xcodebuild test-without-building -project $PROJECT_PATH -scheme $SCHEME -sdk iphonesimulator -destination "${DESTINATION}"
+# Build the project and run the tests.
+xcodebuild build-for-testing -project $PROJECT_PATH -scheme $SCHEME -sdk iphonesimulator -destination "platform=iOS Simulator,id=${SIMULATOR_UDID}"
+xcodebuild test-without-building -project $PROJECT_PATH -scheme $SCHEME -sdk iphonesimulator -destination "platform=iOS Simulator,id=${SIMULATOR_UDID}"
+# Remove the simulator we created earlier.
 xcrun simctl delete $SIMULATOR_UDID
