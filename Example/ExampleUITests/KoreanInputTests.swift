14f0cdd818e63aa042452ce1359e48cb8aa88d96
Simon Støvring <mail@simonbs.dk>
2022-11-06 17:50:39 +0100

    Moves helpers to XCUIApplication+Helpers

--- Example/ExampleUITests/KoreanInputTests.swift
+++ Example/ExampleUITests/KoreanInputTests.swift
@@ -126,16 +126,3 @@ final class KoreanInputTests: XCTestCase {
         XCTAssertEqual(app.textView?.value as? String, "어어어\n\"어어어\"\n어어어")
     }
 }
-
-private extension XCUIApplication {
-    var textView: XCUIElement? {
-        return scrollViews.children(matching: .textView).element
-    }
-
-    func tap(at point: CGPoint) {
-        let normalized = coordinate(withNormalizedOffset: .zero)
-        let offset = CGVector(dx: point.x, dy: point.y)
-        let coordinate = normalized.withOffset(offset)
-        coordinate.tap()
-    }
-}
--- Example/ExampleUITests/XCUIApplication+Helpers.swift
+++ Example/ExampleUITests/XCUIApplication+Helpers.swift
@@ -5,6 +5,17 @@ private enum EnvironmentKey {
 }
 
 extension XCUIApplication {
+    var textView: XCUIElement? {
+        return scrollViews.children(matching: .textView).element
+    }
+
+    func tap(at point: CGPoint) {
+        let normalized = coordinate(withNormalizedOffset: .zero)
+        let offset = CGVector(dx: point.x, dy: point.y)
+        let coordinate = normalized.withOffset(offset)
+        coordinate.tap()
+    }
+
     func disablingTextPersistance() -> Self {
         var newLaunchEnvironment = launchEnvironment
         newLaunchEnvironment[EnvironmentKey.disableTextPersistance] = "1"
