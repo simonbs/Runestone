0440c21881605f4088bfc4a13bf13c1631a783b7
Simon Støvring <mail@simonbs.dk>
2023-01-29 16:32:58 +0100

    Makes iOS TextView available on iOS-only

--- Sources/Runestone/TextView/Core/iOS/TextView_iOS.swift
+++ Sources/Runestone/TextView/Core/iOS/TextView_iOS.swift
@@ -1,3 +1,4 @@
+#if os(iOS)
 // swiftlint:disable file_length type_body_length
 import CoreText
 import UIKit
@@ -1318,3 +1319,4 @@ extension TextView: HighlightNavigationControllerDelegate {
         }
     }
 }
+#endif
