25c1566f31a6c7c099551980906a9fc88bf7ef0c
Simon Støvring <mail@simonbs.dk>
2023-01-29 16:31:33 +0100

    Makes NSEdgeInsets conform to Equatable

--- Sources/Runestone/MultiPlatform/MultiPlatformEdgeInsets.swift
+++ Sources/Runestone/MultiPlatform/MultiPlatformEdgeInsets.swift
@@ -12,4 +12,10 @@ extension NSEdgeInsets {
         NSEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
+
+extension NSEdgeInsets: Equatable {
+    public static func == (lhs: NSEdgeInsets, rhs: NSEdgeInsets) -> Bool {
+        lhs.left == rhs.left && lhs.top == rhs.top && lhs.right == rhs.right && lhs.bottom == rhs.bottom
+    }
+}
 #endif
