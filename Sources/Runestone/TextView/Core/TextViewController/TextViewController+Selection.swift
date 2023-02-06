dcd585bc425bd095966b5708679251eb3d8e7132
Simon Støvring <mail@simonbs.dk>
2023-02-06 07:41:46 +0100

    Fixes line selection

--- Sources/Runestone/TextView/Core/TextViewController/TextViewController+Selection.swift
+++ Sources/Runestone/TextView/Core/TextViewController/TextViewController+Selection.swift
@@ -3,10 +3,12 @@ import Foundation
 
 extension TextViewController {
     func moveLeftAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(by: .character, inDirection: .backward)
     }
 
     func moveRightAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(by: .character, inDirection: .forward)
     }
 
@@ -19,34 +21,42 @@ extension TextViewController {
     }
 
     func moveWordLeftAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(by: .word, inDirection: .backward)
     }
 
     func moveWordRightAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(by: .word, inDirection: .forward)
     }
 
     func moveToBeginningOfLineAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(toBoundary: .line, inDirection: .backward)
     }
 
     func moveToEndOfLineAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(toBoundary: .line, inDirection: .forward)
     }
 
     func moveToBeginningOfParagraphAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(toBoundary: .paragraph, inDirection: .backward)
     }
 
     func moveToEndOfParagraphAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(toBoundary: .paragraph, inDirection: .forward)
     }
 
     func moveToBeginningOfDocumentAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(toBoundary: .document, inDirection: .backward)
     }
 
     func moveToEndOfDocumentAndModifySelection() {
+        navigationService.resetPreviousLineMovementOperation()
         move(toBoundary: .document, inDirection: .forward)
     }
 }
@@ -54,14 +64,12 @@ extension TextViewController {
 private extension TextViewController {
     private func move(by granularity: TextGranularity, inDirection directon: TextDirection)  {
         if let currentlySelectedRange = selectedRange {
-            navigationService.resetPreviousLineMovementOperation()
             selectedRange = selectionService.range(movingFrom: currentlySelectedRange, by: granularity, inDirection: directon)
         }
     }
 
     private func move(toBoundary boundary: TextBoundary, inDirection directon: TextDirection) {
         if let currentlySelectedRange = selectedRange {
-            navigationService.resetPreviousLineMovementOperation()
             selectedRange = selectionService.range(movingFrom: currentlySelectedRange, toBoundary: boundary, inDirection: directon)
         }
     }
