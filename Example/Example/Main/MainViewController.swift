ca857b689a652231d598a0556c6c71af7bc95113
Simon Støvring <mail@simonbs.dk>
2023-08-16 21:44:50 +0100

    Improves formatting

--- Example/Example/Main/MainViewController.swift
+++ Example/Example/Main/MainViewController.swift
@@ -15,14 +15,18 @@ final class MainViewController: UIViewController {
         toolsView = KeyboardToolsView(textView: contentView.textView)
         super.init(nibName: nil, bundle: nil)
         title = "Example"
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillChangeFrame(_:)),
-                                               name: UIApplication.keyboardWillChangeFrameNotification,
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide(_:)),
-                                               name: UIApplication.keyboardWillHideNotification,
-                                               object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillChangeFrame(_:)),
+            name: UIApplication.keyboardWillChangeFrameNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIApplication.keyboardWillHideNotification,
+            object: nil
+        )
     }
 
     required init?(coder: NSCoder) {
