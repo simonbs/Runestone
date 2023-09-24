dc28155b389132b96cde51eeb8274deda1b7ddb4
Simon Støvring <mail@simonbs.dk>
2023-09-24 17:49:34 +0200

    Fixes cursor hidden when marking text

---
+++ Sources/Runestone/Library/UITextSelectionDisplayInteraction+Helpers.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+
+@available(iOS 17, *)
+extension UITextSelectionDisplayInteraction {
+    func sbs_enableCursorBlinks() {
+        setValue(true, forKey: "rosruc".reversed() + "Blinks")
+    }
+}
+
