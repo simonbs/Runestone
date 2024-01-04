485a1f593a31b5c624e612a420dbeadb88f02c67
Simon Støvring <mail@simonbs.dk>
2024-01-04 18:42:35 +0100

    Removes LineFragmentTree

--- Sources/Runestone/Core/LineController/LineFragmentTree/LineFragmentNodeData.swift
+++
@@ -1,6 +0,0 @@
-import CoreGraphics
-
-final class LineFragmentNodeData {
-    var lineFragmentHeight: CGFloat = 0
-    var totalLineFragmentHeight: CGFloat = 0
-}
--- Sources/Runestone/Core/LineController/LineFragmentTree/LineFragmentTree.swift
+++
@@ -1,3 +0,0 @@
-import _RunestoneRedBlackTree
-
-typealias LineFragmentTree = RedBlackTree<Int, LineFragmentNodeData>
