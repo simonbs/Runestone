392e97fd08b574cd15edc2e963c659215a4f5e60
Simon B. Støvring <mail@simonbs.dk>
2024-03-16 16:11:32 +0100

    Adds DefaultStringAttributes (#359)

--- Sources/Runestone/TextView/LineController/LineSyntaxHighlighter.swift
+++ Sources/Runestone/TextView/LineController/LineSyntaxHighlighter.swift
@@ -23,24 +23,8 @@ final class LineSyntaxHighlighterInput {
 protocol LineSyntaxHighlighter: AnyObject {
     typealias AsyncCallback = (Result<Void, Error>) -> Void
     var theme: Theme { get set }
-    var kern: CGFloat { get set }
     var canHighlight: Bool { get }
-    func setDefaultAttributes(on attributedString: NSMutableAttributedString)
     func syntaxHighlight(_ input: LineSyntaxHighlighterInput)
     func syntaxHighlight(_ input: LineSyntaxHighlighterInput, completion: @escaping AsyncCallback)
     func cancel()
-}
-
-extension LineSyntaxHighlighter {
-    func setDefaultAttributes(on attributedString: NSMutableAttributedString) {
-        let entireRange = NSRange(location: 0, length: attributedString.length)
-        let attributes: [NSAttributedString.Key: Any] = [
-            .foregroundColor: theme.textColor,
-            .font: theme.font,
-            .kern: kern as NSNumber
-        ]
-        attributedString.beginEditing()
-        attributedString.setAttributes(attributes, range: entireRange)
-        attributedString.endEditing()
-    }
 }
