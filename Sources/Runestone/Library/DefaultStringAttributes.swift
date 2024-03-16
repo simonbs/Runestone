c09a14f96bbe66253a4d7a16faa1cea962ae6c93
Simon B. Støvring <mail@simonbs.dk>
2024-03-16 16:51:08 +0100

    Removes lineHeightMultiplier from DefaultStringAttributes (#361)
    * Removes lineHeightMultiplier from DefaultStringAttributes
    
    * Removes initializer

--- Sources/Runestone/Library/DefaultStringAttributes.swift
+++ Sources/Runestone/Library/DefaultStringAttributes.swift
@@ -2,31 +2,15 @@ import Foundation
 import UIKit
 
 struct DefaultStringAttributes {
-    private let textColor: UIColor
-    private let font: UIFont
-    private let kern: CGFloat
-    private let tabWidth: CGFloat
-    private let lineHeightMultiplier: CGFloat
-
-    init(
-        textColor: UIColor,
-        font: UIFont,
-        kern: CGFloat,
-        tabWidth: CGFloat,
-        lineHeightMultiplier: CGFloat = 1
-    ) {
-        self.textColor = textColor
-        self.font = font
-        self.kern = kern
-        self.tabWidth = tabWidth
-        self.lineHeightMultiplier = lineHeightMultiplier
-    }
+    let textColor: UIColor
+    let font: UIFont
+    let kern: CGFloat
+    let tabWidth: CGFloat
 
     func apply(to attributedString: NSMutableAttributedString) {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.tabStops = []
         paragraphStyle.defaultTabInterval = tabWidth
-        paragraphStyle.lineHeightMultiple = lineHeightMultiplier
         let range = NSRange(location: 0, length: attributedString.length)
         let attributes: [NSAttributedString.Key: Any] = [
             .foregroundColor: textColor,
