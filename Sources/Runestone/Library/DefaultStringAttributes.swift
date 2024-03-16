ed2f83f59bcbf248bcd573f583bf2b239ce0f4cd
Simon B. Støvring <mail@simonbs.dk>
2024-03-16 17:20:57 +0100

    Adds default tab stops (#362)
    * Adds default tab stops
    
    * Fixes SwiftLint warning

--- Sources/Runestone/Library/DefaultStringAttributes.swift
+++ Sources/Runestone/Library/DefaultStringAttributes.swift
@@ -9,7 +9,9 @@ struct DefaultStringAttributes {
 
     func apply(to attributedString: NSMutableAttributedString) {
         let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.tabStops = []
+        paragraphStyle.tabStops = (0 ..< 20).map { index in
+            NSTextTab(textAlignment: .natural, location: CGFloat(index) * tabWidth)
+        }
         paragraphStyle.defaultTabInterval = tabWidth
         let range = NSRange(location: 0, length: attributedString.length)
         let attributes: [NSAttributedString.Key: Any] = [
